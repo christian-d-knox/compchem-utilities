af48d0ff57d98a1b155406c767d28c9e29786f02
christian-d-knox <cdk67@pitt.edu>
2024-09-25 13:59:34 -0400

    First version of compUtils

---
+++ compUtils.py
@@ -0,0 +1,9 @@
+# Welcome to Computational Chemistry Utilities v0.1
+# This package has been crafted through the lovely masochism of Christian Drew Knox, for the Peng Liu Group
+# Last major commit to the project was 2024-09-24
+
+# Imports the various libraries needed for main
+import os
+import posix
+import posixpath
+
