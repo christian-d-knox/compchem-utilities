58f9ee076834a522c82ae415e86e9c5ec03180ee
christian-d-knox <cdk67@pitt.edu>
2025-07-24 14:32:31 -0400

    Created a basic installer file to ensure dependencies are properly installed.

---
+++ installer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Welcome to the Computational Chemistry Utilities Installer!
+# This utility serves to install all potentially important modules that compUtils (and normal work) leverages!
+# Written by Christian Drew Knox, for the Peng Liu Research Group
+# Last major commit: 2025-07-24
+
+import os
+
+os.system("pip install numpy")
+os.system("pip install pandas")
+os.system("pip install matplotlib")
+os.system("pip install scikit-learn")
+os.system("pip install scipy")
+os.system("pip install goodvibes")
